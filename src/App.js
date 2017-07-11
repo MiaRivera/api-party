035bef131f0201cd3fd6baec973542fe7ba2a519
Mia Rivera <riverami@iupui.edu>
2017-07-11 19:03:08 -0400

    Add Nasa component and route

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import {Route, Switch, NavLink} from 'react-router-dom'
 import Github from './Github'
+import Nasa from './Nasa'
 
 class App extends Component {
   render() {
@@ -18,9 +19,13 @@ class App extends Component {
           <li>
             <NavLink to="/github">Github API</NavLink>
           </li>
+          <li>
+            <NavLink to="/nasa">NASA API</NavLink>
+          </li>
         </ul>
         <Switch>
           <Route path="/github" component={Github} />
+          <Route path="/nasa" component={Nasa} />
           <Route render={() => <p>To get started, click one of the links above</p>} />
         </Switch>
       </div>
